d7930339d4023650d73417594bfecd5b49925ad2
Keegan Campbell <me@kfcampbell.com>
2022-11-16 15:05:46 -0800

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Creates PRs to add CodeQL and Dependabot workflows across repositories
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
